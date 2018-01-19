22b418904657a4d9010fd32c766b3a392c9f8784
Andrew S. Arnold <andrew.s.arnold@gmail.com>
2018-01-19 15:10:27 -0500

    New WPF UI

---
+++ NewWave.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using NewWave.Generator;
+using NewWave.Generator.Parameters;
+
+namespace NewWave.Wpf
+{
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			Title = $"{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductName} {Assembly.GetExecutingAssembly().GetName().Version}";
+		}
+
+		private void Export(object sender, RoutedEventArgs e)
+		{
+			var song = new GeneratedSong();
+			song.Generate(new ParameterList());
+			var score = song.Render();
+			var outputPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyMusic)}\{DateTime.Now:yyyyMMddhhmmss}.mid";
+			score.ExportMidi(outputPath);
+			Process.Start(outputPath);
+		}
+	}
+}
